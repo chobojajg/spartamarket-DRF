8853b962518aa3b1492a923b32e3caa23911d1bf
Kwan Jeon <jeonkwan100@gmail.com>
2024-05-02 12:18:37 +0900

    로그아웃만 구현...

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 
 
 urlpatterns = [
     path("", views.SignUpAPIView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("logout/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("<str:username>/", views.ProfileAPIView.as_view(), name="profile"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .serializers import UserSerializer
 from rest_framework import status
 from django.contrib.auth.hashers import make_password
@@ -20,6 +23,8 @@ class SignUpAPIView(APIView):
 class ProfileAPIView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request, username):
-        profile = get_object_or_404(User, username=username)
-        serializer = UserSerializer(profile)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if str(request.user) == username:
+            profile = get_object_or_404(User, username=username)
+            serializer = UserSerializer(profile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
